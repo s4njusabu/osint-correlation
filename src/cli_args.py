ed368b317d6136c8c76047573569f4aa75f69d42
Sanju Sabu <sanjusabu.dev@gmail.com>
2026-08-13 13:25:26 +0530

    restructure: put the cli_args() in a seperate file than just dumping it all in main.py

---
+++ src/cli_args.py
@@ -0,0 +1,36 @@
+import argparse, sys
+from email_validator import validate_email, EmailNotValidError
+
+def cli_args():
+    parser = argparse.ArgumentParser(
+        description="""OSINT tool with similarity checks.
+
+Username search : command username s4njusabu
+Email search    : command email sanjusabu@icloud.com""",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument("action", help='Example: "username" or "email"')
+    parser.add_argument("target", help='Example: "s4njusabu" or "sanjusabu@icloud.com". In other words the username or email you wanna search')
+
+    args = parser.parse_args()
+
+    action = args.action.lower()
+
+    if action not in ["username", "email"]:
+        print('Action should be either "username" or "email"')
+        sys.exit(1)
+
+    if action == "email":
+        try:
+            email = validate_email(args.target)
+        except EmailNotValidError as e:
+            print(f"Invalid email: {e}")
+            sys.exit(1)
+    else:
+        username = args.target
+
+    if action == "username":
+        return (action, username)
+    else:
+        return (action, email.normalized)
--- src/main.py
+++ src/main.py
@@ -1,49 +1,17 @@
-import argparse, sys
-from email_validator import validate_email, EmailNotValidError
+from cli_args import cli_args
 
-# Basic structure of the application
+# step 1
+# ------------------------------------------------------------------------
+# BASIC STRUCTURE OF THE APPLICATION
 
 # command [type] [target]
-# example:
+# examples:
 
 #    command username s4njusabu
 #    command email sanjusabu@icloud.com
 
-# ----- first iteration of the app will only include username search ----- 
-
-def cli_args():
-    parser = argparse.ArgumentParser(
-        description="""OSINT tool with similarity checks.
-
-Username search : command username s4njusabu
-Email search    : command email sanjusabu@icloud.com""",
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-
-    parser.add_argument("action", help='Example: "username" or "email"')
-    parser.add_argument("target", help='Example: "s4njusabu" or "sanjusabu@icloud.com". In other words the username or email you wanna search')
-
-    args = parser.parse_args()
-
-    action = args.action.lower()
-
-    if action not in ["username", "email"]:
-        print('Action should be either "username" or "email"')
-        sys.exit(1)
-
-    if action == "email":
-        try:
-            email = validate_email(args.target)
-        except EmailNotValidError as e:
-            print(f"Invalid email: {e}")
-            sys.exit(1)
-    else:
-        username = args.target
-
-    if action == "username":
-        return (action, username)
-    else:
-        return (action, email.normalized)
+# first iteration of the app will only include username search 
+# ------------------------------------------------------------------------
 
 if __name__ == "__main__":
     print(cli_args())
