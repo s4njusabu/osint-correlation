c229bcce0a6bcdc594aac4b839891a0de519e63a
Sanju Sabu <sanjusabu.dev@gmail.com>
2026-08-15 20:44:39 +0530

    Add skeletons of text and image functions

--- src/compare.py
+++ src/compare.py
@@ -1,2 +1,8 @@
 def compare_dates(date1: str, date2: str):
-    print("Hehe")
+    print("Never gonna give you up")
+
+def compare_texts(text1: str, text2: str):
+    print("Never gonna let you down")
+
+def compare_images(image1, image2):
+    print("Never gonna run around and desert you")
